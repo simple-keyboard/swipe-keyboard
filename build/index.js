ffc75bfd983c14ed8bc7a881dd4dc27211bb514f
Francisco Hodge <hello@franciscohodge.com>
2022-04-06 08:05:34 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   swipe-keyboard v2.2.400
+ *   swipe-keyboard v2.2.401
  *   https://github.com/hodgef/swipe-keyboard
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
